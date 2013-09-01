c0e23fe0380a68493f7456d09af6a510973c9b85
Panov Stanislav <panov.st@gmail.com>
2013-09-01 10:43:17 +0400

    init commit

---
+++ src/main/WEB-INF/js/region-selector-model.js
@@ -0,0 +1,93 @@
+/**
+ * Класс-модель
+ * @class
+ * @name RegionSelector.Model
+ */
+RegionSelector.Model = function () {
+    this.events = new ymaps.event.Manager();
+    this.options = new ymaps.option.Manager({
+        preset: this.getDefaults()
+    });
+    this._monitor = new ymaps.Monitor(this.options);
+
+    this._setupMonitor();
+};
+
+/**
+ * @lends RegionSelector.Model.prototype
+ */
+RegionSelector.Model.prototype = {
+    /**
+     * @contructor
+     */
+    constructor: RegionSelector.Model,
+    /**
+     * Используем ymaps.Monitor для наблюдения за изменениями опций модели.
+     * @function
+     * @private
+     * @name RegionSelector.Model._setupMonitor
+     */
+    _setupMonitor: function () {
+        this._monitor
+            .add(['country', 'lang', 'quality'], function (newValues, oldValues) {
+                /**
+                 * Хак для смены языка при смене страны.
+                 */
+                if(newValues.country !== oldValues.country) {
+                    this.options.unset('lang');
+                }
+                this.load();
+            }, this, this.getDefaults());
+    },
+    /**
+     * Отключение мониторинга опций модели.
+     * @function
+     * @private
+     * @name RegionSelector.Model._clearMonitor
+     */
+    _clearMonitor: function () {
+        this._monitor
+            .removeAll();
+    },
+    /**
+     * Загружаем данные.
+     * @function
+     * @name RegionSelector.Model.load
+     */
+    load: function () {
+        ymaps.regions.load(
+            this.options.get('country'), {
+                lang: this.options.get('lang'),
+                quality: this.options.get('quality')
+            }
+        ).then(
+            ymaps.util.bind(this._onDataLoaded, this)
+        );
+    },
+    /**
+     * Обработчик загрузки данных.
+     * @function
+     * @private
+     * @name RegionSelector.Model._onDataLoaded
+     * @param {Object} data Данные региона.
+     */
+    _onDataLoaded: function (data) {
+        this.events.fire('load', {
+            regions: data.geoObjects,
+            target: this
+        });
+    },
+    /**
+     * Опции модели по-умолчанию.
+     * @function
+     * @name RegionSelector.Model.getDefaults
+     * @returns {Object} Опции модели.
+     */
+    getDefaults: function () {
+        return {
+            country: 'RU',
+            lang: 'ru',
+            quality: 0
+        };
+    }
+};
